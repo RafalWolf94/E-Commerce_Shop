c0b29c296f1cb2484b849b6db127d3064f630f4e
Rafał Wolschlaeger <50140746+RafalWolf94@users.noreply.github.com>
2021-05-22 10:18:22 +0200

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("apo/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "Single product";
+            
+        }
+    }
+}
