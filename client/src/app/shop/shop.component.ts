adfcea2e24efc1482a142995807e79ae120caca4
Rafał Wolschlaeger <50140746+RafalWolf94@users.noreply.github.com>
2021-06-14 01:42:04 +0200

    Added searching, and resolved pagination bug, also added pagination model and component

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ShopService} from "./shop.service";
 import {IProduct} from "../shared/models/product";
 import {IType} from "../shared/models/productType";
@@ -11,6 +11,7 @@ import {ShopParams} from "../shared/models/shopParams";
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild('search', {static: false}) searchTerm: ElementRef;
   products: IProduct[]
   brands: IBrand[]
   types: IType[]
@@ -61,11 +62,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -75,8 +78,23 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanges(event: any) {
-    this.shopParams.pageNumber = event.page;
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+
 
 }
