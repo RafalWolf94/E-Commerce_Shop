a2665bf4eeff30ac9b9c5027023b08c2a03e865b
Rafał Wolschlaeger <50140746+RafalWolf94@users.noreply.github.com>
2021-06-18 23:09:27 +0200

    Added Redis And Basket

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository BasketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            BasketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketId(string id)
+        {
+            var basket = await BasketRepository.GetBasketAsync(id);
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updateBasket = await BasketRepository.UpdateBasketAsync(basket);
+            return Ok(updateBasket);
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasketAsync(string id) => await BasketRepository.DeleteBasketAsync(id);
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -24,15 +25,14 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(Config.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(Config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
-            services.AddCors(option =>
-            {
-                option.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
-                });
-            });
+            services.AddCors(option => { option.AddPolicy("CorsPolicy", policy => { policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"); }); });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
